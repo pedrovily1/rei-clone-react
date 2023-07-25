9538ccb6e739821ebaf5fc4acd932dc62617cadd
BAL17 <126348686+BAL17@users.noreply.github.com>
2023-07-25 16:13:00 -0500

    lowerbar changes needs js functionality

--- src/components/header/LowerBar.jsx
+++ src/components/header/LowerBar.jsx
@@ -2,26 +2,26 @@ import './LowerBar.css';
 
 const LowerBar = () => {
     return (
-        <>
-            <hr></hr>
-            <div id="LowerBar">
-                <div>Camp & Hike</div>
-                <div>Climb</div>
-                <div>Cycle</div>
-                <div>Water</div>
-                <div>Run</div>
-                <div>Fitness</div>
-                <div>Snow</div>
-                <div>Travel</div>
-                <div>Men</div>
-                <div>Women</div>
-                <div>Kids</div>
-                <div>Deals</div>
-                <div>Brands</div>
-                <div>More</div>
-            </div>
-        </>
-    )
+      <>
+        <hr id="lowerBarhr"></hr>
+        <div className="LowerBar">
+          <div id="lowerBarBtns">Camp & Hike</div>
+          <div id="lowerBarBtns">Climb</div>
+          <div id="lowerBarBtns">Cycle</div>
+          <div id="lowerBarBtns">Water</div>
+          <div id="lowerBarBtns">Run</div>
+          <div id="lowerBarBtns">Fitness</div>
+          <div id="lowerBarBtns">Snow</div>
+          <div id="lowerBarBtns">Travel</div>
+          <div id="lowerBarBtns">Men</div>
+          <div id="lowerBarBtns">Women</div>
+          <div id="lowerBarBtns">Kids</div>
+                <div id="lowerBarBtns" style={{ color: 'red', borderColor: 'red'}}>Deals</div>
+          <div id="lowerBarBtns">Brands</div>
+          <div id="lowerBarBtns">More</div>
+        </div>
+      </>
+    );
 }
 
 export default LowerBar;
