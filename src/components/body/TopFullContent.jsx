ee86f69f59a31819b004c2860fd95bc440f95146
BAL17 <126348686+BAL17@users.noreply.github.com>
2023-07-26 17:10:19 -0500

    updated fullcontent and below the fold build

--- src/components/body/TopFullContent.jsx
+++ src/components/body/TopFullContent.jsx
@@ -3,26 +3,26 @@ import "./TopFullContent.css";
 const TopFullContent = () => {
   return (
     <div id="topFullContainer">
-      <nav>
-        <ol>
-          <li>
+      <nav className="navComp">
+        <ul className="productHeading">
+          <li className="productDescText">
             <a href="https://www.rei.com/c/camping-and-hiking">...</a>
-            <span>/</span>
+            <span className="slashDivider">/</span>
           </li>
-          <li>
+          <li className="productDescText">
             <a href="https://www.rei.com/c/dog-gear">Dog Gear</a>
-            <span>/</span>
+            <span className="slashDivider">/</span>
           </li>
-          <li>
+          <li className="productDescText">
             <a href="https://www.rei.com/c/dog-beds-and-carriers">
               Dog Beds and Carriers
             </a>
-            <span>/</span>
+            <span className="slashDivider">/</span>
           </li>
           <li>
             <a href="https://www.rei.com/c/dog-carriers">Dog Carriers</a>
           </li>
-        </ol>
+        </ul>
       </nav>
     </div>
   );
