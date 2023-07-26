b62d82dd172951e5648f327e416847e4bac62b2c
Isaac Summers <monogator@gmail.com>
2023-07-26 09:53:54 -0700

    Created baseline body components

---
+++ src/components/body/BodyContainer.jsx
@@ -0,0 +1,28 @@
+import './BodyContainer.css'
+import TopFullContent from './TopFullContent'
+import ImageContainer from './ImageContainer'
+import ProductHeader from './ProductHeader'
+import BuyBox from './BuyBox'
+import BelowTheFold from './BelowTheFold'
+
+
+const BodyContainer = () => {
+    return (
+        <div id='bodyContainer'>
+            <TopFullContent />
+            <ImageContainer />
+            <ProductHeader />
+            <BuyBox />
+            <BelowTheFold />
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+export default BodyContainer
