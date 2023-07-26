fa62a923ecac7b9d2e3d591d4a26f7b7be1e1e21
pedro modesto <pedrovily67@gmail.com>
2023-07-26 18:29:51 -0400

    message

--- src/components/body/RatingId.jsx
+++ src/components/body/RatingId.jsx
@@ -3,13 +3,45 @@ import "./RatingId.css";
 const RatingId = () => {
   return (
     <>
-      <div id="phRating">
-        <a href="https://www.rei.com/product/186314/timbuk2-muttmover-luxe-pack#">
-          <div>stars go here</div>
-          <span>4.1 | 9 reviews</span>
-        </a>
+      <div className="product-details">
+        <div className="product-rating">
+
+
+
+          <div className="stars">
+            <div className="star">
+              <img src="src\assets\Star.svg" alt="Icon Description"></img>
+            </div>
+            <div className="star">
+              <img src="/src\assets\Star.svg" alt="Icon Description"></img>
+            </div>
+            <div className="star">
+              <img src="src\assets\Star.svg" alt="Icon Description"></img>
+            </div>
+            <div className="star">
+              <img src="src\assets\Star.svg" alt="Icon Description"></img>
+            </div>
+            <div className="star empty">
+              <img src="src\assets\Starempty.svg" alt="Icon Description"></img>
+            </div>
+            <a href="https://www.rei.com/product/186314/timbuk2-muttmover-luxe-pack#">
+              <div className="rating-details">
+                <div className="rating-value">4.1</div>
+                <div className="rating-count">9</div>
+                <div className="rating-count">Reviews</div>
+              </div>
+            </a>
+
+
+
+            <div className="product-id">
+              <div  >Item</div>
+              <div  >#186314</div>
+            </div>
+          </div>
+
+        </div>
       </div>
-      <span>Item #186314</span>
     </>
   );
 };
