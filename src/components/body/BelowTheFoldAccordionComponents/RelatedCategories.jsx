b320e5cac06366c7847bfe2c48f8f19c26d9dee0
BAL17 <126348686+BAL17@users.noreply.github.com>
2023-07-28 17:49:13 -0500

    Related Categories 98.9% completed

--- src/components/body/BelowTheFoldAccordionComponents/RelatedCategories.jsx
+++ src/components/body/BelowTheFoldAccordionComponents/RelatedCategories.jsx
@@ -1,21 +1,70 @@
 import "./RelatedCategories.css";
+import Search from "/src/assets/Search.svg";
 
 const RelatedCategories = () => {
   return (
     <>
       <nav>
-        <h2>Related Categories</h2>
-        <ul>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-          <li></li>
-        </ul>
+        <h2 className="theTitle">Related Categories</h2>
+        <div className="relatedBtnDiv">
+          <button className="relatedBtns">
+            <a
+              className="aForRelatedBtns"
+              href="https://www.rei.com/c/dog-gear/f/scd-deals"
+            >
+              <img src={Search} alt=""></img>
+              Dog Gear: Deals
+            </a>
+          </button>
+          <button className="relatedBtns">
+            <a
+              className="aForRelatedBtns"
+              href="https://www.rei.com/c/boots/f/f-waterproof"
+            >
+              <img src={Search} alt=""></img>
+              Waterproof Boots
+            </a>
+          </button>
+          <button className="relatedBtns">
+            <a
+              className="aForRelatedBtns"
+              href="https://www.rei.com/b/salomon/c/hiking-footwear"
+            >
+              <img src={Search} alt=""></img>
+              Salomon Hiking Footwear
+            </a>
+          </button>
+          <button className="relatedBtns">
+            <a
+              className="aForRelatedBtns"
+              href="https://www.rei.com/c/kids-hiking-shoes"
+            >
+              <img src={Search} alt=""></img>
+              Kids' Hiking Shoes
+            </a>
+          </button>
+          <button className="relatedBtns">
+            <a
+              className="aForRelatedBtns"
+              href="https://www.rei.com/c/dog-packs"
+            >
+              <img src={Search} alt=""></img>
+              Dog Packs
+            </a>
+          </button>
+          <button className="relatedBtns">
+            <a
+              className="aForRelatedBtns"
+              href="https://www.rei.com/b/vasque/c/footwear"
+            >
+              <img src={Search} alt=""></img>
+              Vasque Footwear
+            </a>
+          </button>
+        </div>
       </nav>
     </>
   );
-}; 
+};
 
 export default RelatedCategories;
